002d049984702aaa846a4c7572c71f9301d542c3
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 16:14:19 -0300

    create page tecnology

---
+++ src/pages/Tecnology/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../../components/Container';
+
+const Tecnology = (props) => {
+  return (
+    <>
+      <Container>
+        <p>home page</p>
+      </Container>
+    </>
+  );
+};
+
+export default Tecnology;
