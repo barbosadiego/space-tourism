db44affacc0bb3605d875e782e58d04bf6073469
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 14:16:13 -0300

    add browser router to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import GlobalStyles from './styles/GlobalStyles.js';
 import Theme from './theme/theme.js';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme>
       <GlobalStyles />
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Theme>
   </React.StrictMode>,
 );
