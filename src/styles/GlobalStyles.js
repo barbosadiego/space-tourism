11d26da627671e2d07da8e035a06e30927f30443
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 11:17:51 -0300

    create global styles file

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+import pxToRem from '../helper/tipographySwitch';
+
+const GlobalStyles = createGlobalStyle`
+  *, *::after, *::before{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background-color: ${({ theme }) => theme.colors.primary};
+  }
+
+  h1, h2, h3, h4, h5, p{
+    font-family: "Bellefair", sans-serif;
+    text-transform: uppercase;
+    font-weight: 400;
+    color: ${({ theme }) => theme.colors.white};
+  }
+
+  h1{
+    font-size: ${pxToRem(150)};
+    line-height: ${pxToRem(171)};
+  }
+
+  h2{
+    font-size: ${pxToRem(100)};
+    line-height: ${pxToRem(115)};
+  }
+
+  h3{
+    font-size: ${pxToRem(56)};
+    line-height: ${pxToRem(64)};
+  }
+
+  h4{
+    font-size: ${pxToRem(32)};
+    line-height: ${pxToRem(37)};
+  }
+
+  h5{
+    font-family: 'Barlow Condensed', sans-serif;
+    font-size: ${pxToRem(28)};
+    line-height: ${pxToRem(32)};
+    letter-spacing: ${pxToRem(4.75)};
+  }
+
+  p{
+    font-family: 'Barlow Condensed', sans-serif;
+    font-size: ${pxToRem(18)};
+    font-weight: 400;
+    line-height: ${pxToRem(32)};
+    letter-spacing: ${pxToRem(2.7)};
+  }
+`;
+
+export default GlobalStyles;
