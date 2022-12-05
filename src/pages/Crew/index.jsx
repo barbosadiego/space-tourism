58b72f3b9101e5e82b3d98cdf9034167e8a2cd12
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 16:13:38 -0300

    create page crew

---
+++ src/pages/Crew/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../../components/Container';
+
+const Crew = (props) => {
+  return (
+    <>
+      <Container>
+        <p>home page</p>
+      </Container>
+    </>
+  );
+};
+
+export default Crew;
