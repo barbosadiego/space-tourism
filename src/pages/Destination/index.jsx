9928f529c00756305dd330100630fa4a355f0d4d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 16:13:56 -0300

    create page destination

---
+++ src/pages/Destination/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../../components/Container';
+
+const Destination = (props) => {
+  return (
+    <>
+      <Container>
+        <p>home page</p>
+      </Container>
+    </>
+  );
+};
+
+export default Destination;
