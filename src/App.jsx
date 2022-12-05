c062354303428d96814988f714b112e6a75aa79b
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 14:20:56 -0300

    add routes to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 
 const App = () => {
-  return <div>ola mundo</div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
