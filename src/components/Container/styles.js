0407103664955c13e83855055ecdcf8e26f68779
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 16:14:46 -0300

    create container component

---
+++ src/components/Container/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const StyledContainer = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export default StyledContainer;
