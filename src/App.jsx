3de2e5af1487e40486c459e576171440c3735fce
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-05 16:20:53 -0300

    add new routes in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Crew from './pages/Crew';
+import Destination from './pages/Destination';
 
 import Home from './pages/Home';
+import Technology from './pages/Tecnology';
 
 const App = () => {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </main>
     </>
   );
 };
