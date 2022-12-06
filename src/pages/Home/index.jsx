bea466ecd9804595b0cc67cd9f822be9f20b596d
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-06 15:55:55 -0300

    update home page for mobile

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import Container from '../../components/Container';
+import NavText from '../../components/NavText';
+import MainButton from '../../components/MainButton';
 import StyledHome from './styles';
 
-const Home = ({ isMobile, isDesktop }) => {
-  console.log(isMobile);
+const Home = ({ isMobile, isDesktop, isTablet }) => {
   return (
     <>
-      <Container>
-        <StyledHome isMobile={isMobile} isDesktop={isDesktop}>
-          <p>home page</p>
-        </StyledHome>
-      </Container>
+      <StyledHome isMobile={isMobile} isDesktop={isDesktop} isTablet={isTablet}>
+        <Container>
+          <NavText>SO, YOU WANT TO TRAVEL TO</NavText>
+          <h1>space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+          <MainButton buttonWidth={150} isDesktop={isDesktop}>
+            explore
+          </MainButton>
+        </Container>
+      </StyledHome>
     </>
   );
 };
