287c868ddee36e295c7ad6aec88be740c68ee670
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-06 15:55:26 -0300

    update main button styles

--- src/components/MainButton/styles.js
+++ src/components/MainButton/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import pxToRem from '../../helper/tipographySwitch';
 
 const StyledMainButton = styled.div`
@@ -6,9 +6,11 @@ const StyledMainButton = styled.div`
   width: ${({ buttonWidth }) => buttonWidth + 'px'};
   height: ${({ buttonWidth }) => buttonWidth + 'px'};
   border-radius: 50%;
+  border: 3px solid transparent;
   display: flex;
   align-items: center;
   justify-content: center;
+  font-family: inherit;
   font-size: ${({ buttonWidth }) => buttonWidth / 120 + 'rem'};
   line-height: ${pxToRem(36.7)};
   letter-spacing: ${pxToRem(2)};
@@ -16,10 +18,18 @@ const StyledMainButton = styled.div`
   cursor: pointer;
   transition: 0.3s;
 
-  // only on desktop
-  &:hover {
-    box-shadow: 0 0 0 ${pxToRem(88)} rgba(256, 256, 256, 0.1);
+  &:active {
+    box-shadow: 0 0 0 10px rgba(256, 256, 256, 0.5);
   }
+
+  // only on desktop
+  ${({ isDesktop }) =>
+    isDesktop &&
+    css`
+      &:hover {
+        box-shadow: 0 0 0 ${pxToRem(88)} rgba(256, 256, 256, 0.1);
+      }
+    `}
 `;
 
 export default StyledMainButton;
