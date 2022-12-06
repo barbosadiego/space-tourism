362dd605c278470b2bf3f09d49e16a1c5954a5cf
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-06 15:56:19 -0300

    add styles for mobile in home page

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,24 +2,48 @@ import styled from 'styled-components';
 import bg_mobile from '../../assets/home/background-home-mobile.jpg';
 import tablet_bg_mobile from '../../assets/home/background-home-tablet.jpg';
 import desktop_bg_mobile from '../../assets/home/background-home-desktop.jpg';
+import pxToRem from '../../helper/tipographySwitch';
 
 const StyledHome = styled.section`
-  width: 100%;
+  width: 100vw;
   height: 100vh;
   position: absolute;
   top: 0;
   left: 0;
-  padding: 100px 20px 20px;
-  z-index: -1;
+  padding: 110px 20px 20px;
+  text-align: center;
 
   // bg for mobile
   background-image: ${({ isMobile }) => (isMobile ? `url(${bg_mobile})` : '')};
-  // bg for dektop or tablet
+  // bg for tablet
+  background-image: ${({ isTablet }) =>
+    isTablet ? `url(${tablet_bg_mobile})` : ''};
+  // bg for dektop
   background-image: ${({ isDesktop }) =>
-    isDesktop ? `url(${desktop_bg_mobile})` : `url(${tablet_bg_mobile})`};
+    isDesktop ? `url(${desktop_bg_mobile})` : ''};
 
   background-size: cover;
   background-repeat: no-repeat;
+
+  h1 {
+    font-family: 'Bellefair', sans-serif;
+    font-size: ${pxToRem(80)};
+    line-height: ${pxToRem(100)};
+  }
+
+  p:last-of-type {
+    margin-bottom: 60px;
+    font-size: ${pxToRem(15)};
+    line-height: ${pxToRem(25)};
+  }
+
+  & > div {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 
 export default StyledHome;
