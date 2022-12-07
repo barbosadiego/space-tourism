8f76bd6f428853729cf69005821c4720d8cc81cb
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-07 15:06:26 -0300

    update menu with device type context

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import StyledMenu, { StyledItem } from './styles';
 import Container from '../Container';
 import NavText from '../NavText';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/shared/logo.svg';
+import { WindowContext } from '../../contexts/windowContext';
 
-const Menu = ({ active, setActive, isDesktop }) => {
+const Menu = ({ active, setActive }) => {
+  const { windowData } = useContext(WindowContext);
+
   return (
     <Container>
-      <StyledMenu isDesktop={isDesktop}>
+      <StyledMenu isDesktop={windowData.desktop}>
         <Link to="/">
           <img src={logo} alt="Space Tourism Website logo" />
         </Link>
@@ -16,28 +19,28 @@ const Menu = ({ active, setActive, isDesktop }) => {
         <ul>
           <StyledItem active={active === 1} onClick={() => setActive(1)}>
             <Link to="/">
-              {isDesktop && <span>00</span>}
+              {windowData.desktop && <span>00</span>}
               <NavText>home</NavText>
             </Link>
           </StyledItem>
 
           <StyledItem active={active === 2}>
             <Link to="/destination" onClick={() => setActive(2)}>
-              {isDesktop && <span>01</span>}
+              {windowData.desktop && <span>01</span>}
               <NavText>destination</NavText>
             </Link>
           </StyledItem>
 
           <StyledItem active={active === 3}>
             <Link to="/crew" onClick={() => setActive(3)}>
-              {isDesktop && <span>02</span>}
+              {windowData.desktop && <span>02</span>}
               <NavText>crew</NavText>
             </Link>
           </StyledItem>
 
           <StyledItem active={active === 4}>
             <Link to="/technology" onClick={() => setActive(4)}>
-              {isDesktop && <span>03</span>}
+              {windowData.desktop && <span>03</span>}
               <NavText>technology</NavText>
             </Link>
           </StyledItem>
