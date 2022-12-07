f8744dab4b6ad709032526dd5eeda8c6d9da4d08
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-07 15:46:39 -0300

    add styled dial component in destination page

--- src/pages/Destination/index.jsx
+++ src/pages/Destination/index.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Card from '../../components/Card';
 import Container from '../../components/Container';
+import { WindowContext } from '../../contexts/windowContext';
+import { StyledDial, StyledButton } from './styles';
 
 const Destination = () => {
   const [data, setData] = useState();
+  const [active, setActive] = useState(1);
+  const { windowData } = useContext(WindowContext);
 
   useEffect(() => {
     const getPageInfo = async () => {
@@ -22,6 +26,28 @@ const Destination = () => {
   return (
     <>
       <Container>
+        <StyledDial isDesktop={windowData.desktop}>
+          <li>
+            <StyledButton onClick={() => setActive(1)} active={active === 1}>
+              moon
+            </StyledButton>
+          </li>
+          <li>
+            <StyledButton onClick={() => setActive(2)} active={active === 2}>
+              mars
+            </StyledButton>
+          </li>
+          <li>
+            <StyledButton onClick={() => setActive(3)} active={active === 3}>
+              europa
+            </StyledButton>
+          </li>
+          <li>
+            <StyledButton onClick={() => setActive(4)} active={active === 4}>
+              titan
+            </StyledButton>
+          </li>
+        </StyledDial>
         {data &&
           data.destinations.map((item, index) => (
             <Card item={item} key={index} />
