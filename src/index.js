753d4bcf0038e8fede2a6741702b62ecaa9858eb
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-07 15:04:09 -0300

    add device window context to app

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import App from './App.jsx';
 import GlobalStyles from './styles/GlobalStyles.js';
 import Theme from './theme/theme.js';
 import { BrowserRouter } from 'react-router-dom';
+import { WindowContextProvider } from './contexts/windowContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Theme>
-      <GlobalStyles />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <WindowContextProvider>
+        <GlobalStyles />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </WindowContextProvider>
     </Theme>
   </React.StrictMode>,
 );
