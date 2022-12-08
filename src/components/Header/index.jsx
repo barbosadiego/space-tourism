3ca0ec4a252e4e4d41edba783bb13c44b6f1a69f
diegobarbosa <diego.gutemberg@hotmail.com>
2022-12-08 10:46:27 -0300

    remove not used props in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,30 +6,20 @@ import { WindowContext } from '../../contexts/windowContext';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [active, setActive] = useState(1);
   const { windowData } = useContext(WindowContext);
 
   // desktop and tablet menu
   if (!windowData.mobile)
     return (
       <StyledHeader>
-        <Menu
-          isDesktop={windowData.desktop}
-          active={active}
-          setActive={setActive}
-        />
+        <Menu isDesktop={windowData.desktop} />;
       </StyledHeader>
     );
 
   //mobile menu
   return (
     <StyledHeader>
-      <MobileMenu
-        isMenuOpen={isMenuOpen}
-        setIsMenuOpen={setIsMenuOpen}
-        active={active}
-        setActive={setActive}
-      />
+      <MobileMenu isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
     </StyledHeader>
   );
 };
